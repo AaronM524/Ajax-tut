ab3888fc8c260d3e4ef5182903965d5b15dbc4bd
AaronM524 <144465048+AaronM524@users.noreply.github.com>
2024-10-24 23:50:22 -0400

    Update jqscript.js

--- jqscript.js
+++ jqscript.js
@@ -1,40 +1,41 @@
+$(document).ready(function () {
+    
 let pageCounter = 1;
-const animalContainer = $('animal-info');
-var btn = $('#btn');
+const animalContainer = $("#animal-info");
+const btn = $("#btn");
 
 btn.on('click', function() {
+    $.ajax({
+        url: `https://learnwebcode.github.io/json-example/animals-${pageCounter}.json`,
+        method: 'GET',
+        success: function(data){
+            renderHTML(data)
+            pageCounter++
+            if (pageCounter > 3) {
+                btn.addClass("hide-me");
+            }
+        },
+        error: function(){
+            console.log('Connection Error')
+        }
+    })
+})
 
-  $.ajax({
-    url: `https://learnwebcode.github.io/json-example/animals-${pageCounter}.json`,
-    method: 'GET',
-    success: function(data) {
-      renderHTML(data);
-      pageCounter++;
-      if (pageCounter > 3) {
-        btn.addClass("hide-me"); 
-      }
-    },
-    error: function() {
-    console.log("Connection error")
-    }
-  });
+function renderHTML(data){
+    let htmlString = '';
+    data.forEach(function(animal){
+        htmlString += `<p>${animal.name} is a ${animal.species} that likes to eat`
+        animal.foods.likes.forEach(function(like, index){
+            htmlString += index === 0 ? like : ` and ${like}`
+        })
+        htmlString += ' and dislikes'
+        animal.foods.dislikes.forEach(function(dislikes, index){
+            htmlString += index === 0 ? dislikes : ` and ${dislikes}`
+        })
+        htmlString += '.</p>'
+    });
+    animalContainer.append(htmlString)
+}
 
 });
-
-
-function renderHTML(data) {
-  var htmlString = '';
-  data.forEach(function(animal) {
-    htmlString += `<p>${animal.name} is a ${animal.species} that likes to eat`
-    animal.foods.likes.forEach(function(like, index) {
-      htmlString += index === 0 ? like : ` and ${like}`
-    })
-    htmlString += 'and dislikes'
-    animal.foods.dislikes.forEach(function(dislike, index) {
-      htmlString += index === 0 ? dislike : ` and ${dislike}`
-    })
-    htmlString += '.</p>'
-  })
-  animalContainer.append(htmlString)
-}
 
